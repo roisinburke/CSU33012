f95c78dd5df19fa6d85d2a786bd9b1e607db8184
roisinburke <burker9@tcd.ie>
2020-10-23 10:29:11 +0100

    Update test_LCA.py

--- test_LCA.py
+++ test_LCA.py
@@ -1,24 +1,34 @@
 
 # This is a generalized example, not specific to a test framework
 import unittest
-from LCA import LCA
+import LCA
 
-class LCApyTest():
+class LCApyTest(unittest.TestCase):
 
-    def test_first(self):
+    def test_lca4_5(self):
+        self.assertEqual(LCA.findLCA(LCA.root, 4, 5).key, 2)
 
-        root = LCA(1) 
-        root.left = LCA(2) 
-        root.right = LCA(3) 
-        root.left.left = LCA(4) 
-        root.left.right = LCA(5) 
-        root.right.left = LCA(6) 
-        root.right.right = LCA(7) 
+    def test_lca6_2(self):
+        self.assertEqual(LCA.findLCA(LCA.root, 6, 2).key, 1)
 
-        # The exact assertion call depends on the framework as well
-        self.assertEqual(LCA.findLCA(root, 4, 5).key, 2)
-        self.assertEqual(LCA.findLCA(root, 4, 6).key, 1)
-        self.assertEqual(LCA.findLCA(root, 3, 4).key, 1)
-        self.assertEqual(LCA.findLCA(root, 2, 4).key, 2)
-    # ...
+    def test_lca_none(self):
+        self.assertEqual(LCA.findLCA(None, 4, 5), None)
 
+    def test_lca1_5(self):
+        self.assertEqual(LCA.findLCA(LCA.root, 1, 5).key, 1)
+
+    def test_lca_same(self):
+        self.assertEqual(LCA.findLCA(LCA.root, 1, 1).key, 1)
+
+    def test_lca2_3(self):
+        self.assertEqual(LCA.findLCA(LCA.root, 2, 3).key, 1)
+
+    def test_lca6_7(self):
+        self.assertEqual(LCA.findLCA(LCA.root, 6, 7).key, 3)
+
+    def test_lca_fake(self):
+        self.assertEqual(LCA.findLCA(LCA.root, 8, 8), None)
+
+
+if __name__ == '__main__':
+    unittest.main()
