c96b544aabe8263f1a5fb76e02c00f2ed68c750d
roisinburke <burker9@tcd.ie>
2020-11-03 21:31:18 +0000

    Added tests and made minor changes to LCA
    Changed name of Node object to TreeNode. Increased test coverage.

--- LCA Java/LCA.java
+++ LCA Java/LCA.java
@@ -1,32 +1,13 @@
 public class LCA {
 
-    public static void main(String args[]) {
-        LCA tree = new LCA();
-        tree.root = new Node(1);
-        tree.root.left = new Node(2);
-        tree.root.right = new Node(3);
-        tree.root.left.left = new Node(4);
-        tree.root.left.right = new Node(5);
-        tree.root.right.left = new Node(6);
-        tree.root.right.right = new Node(7);
-       /* System.out.println("LCA(4, 5) = " +
-                tree.findLCA(4, 5).data);
-        System.out.println("LCA(4, 6) = " +
-                tree.findLCA(4, 6).data);
-        System.out.println("LCA(3, 4) = " +
-                tree.findLCA(3, 4).data);
-        System.out.println("LCA(2, 4) = " +
-                tree.findLCA(2, 4).data);   */
-    }
-    //Root of the Binary Tree 
-    Node root;
+    TreeNode root;
 
-    Node findLCA(int n1, int n2)
+    TreeNode findLCA(int n1, int n2)
     {
         return findLCA(root, n1, n2);
     }
 
-    Node findLCA(Node node, int n1, int n2)
+    TreeNode findLCA(TreeNode node, int n1, int n2)
     {
 
         if (node == null)
@@ -35,8 +16,8 @@ public class LCA {
         if (node.data == n1 || node.data == n2)
             return node;
 
-        Node left_lca = findLCA(node.left, n1, n2);
-        Node right_lca = findLCA(node.right, n1, n2);
+            TreeNode left_lca = findLCA(node.left, n1, n2);
+            TreeNode right_lca = findLCA(node.right, n1, n2);
 
         if (left_lca!=null && right_lca!=null)
             return node;
@@ -44,12 +25,10 @@ public class LCA {
         return (left_lca != null) ? left_lca : right_lca;
     }
 }
-class Node {
+class TreeNode {
     int data;
-    Node left, right;
-
-    public Node(int item)
-    {
+    TreeNode left, right;
+    public TreeNode(int item) {
         data = item;
         left = right = null;
     }
--- LCA Java/LCAJavaTest.java
+++ LCA Java/LCAJavaTest.java
@@ -1,33 +1,47 @@
 
 public class LCAJavaTest {
     @Test
-    public void firstTest(){
+    public void testLCA(){
         LCA tree = new LCA();
-        tree.root = new Node(1);
-        tree.root.left = new Node(2);
-        tree.root.right = new Node(3);
-        tree.root.left.left = new Node(4);
-        tree.root.left.right = new Node(5);
-        tree.root.right.left = new Node(6);
-        tree.root.right.right = new Node(7);
+        tree.root = new TreeNode(1);
+        tree.root.left = new TreeNode(2);
+        tree.root.right = new TreeNode(3);
+        tree.root.left.left = new TreeNode(4);
+        tree.root.left.right = new TreeNode(5);
+        tree.root.right.left = new TreeNode(6);
+        tree.root.right.right = new TreeNode(7);
 
         int expected = 2;
-        Assert.assertEquals("LCA(4, 5) = ", expected, tree.findLCA(4, 5).data);
+        assertEquals("LCA(4, 5) = ", expected, tree.findLCA(4, 5).data);
         expected = 1;
-        Assert.assertEquals("LCA(4, 6) = " , expected, tree.findLCA(4, 6).data);
+        assertEquals("LCA(4, 6) = " , expected, tree.findLCA(4, 6).data);
         expected = 1;
-        Assert.assertEquals("LCA(3, 4) = ", expected, tree.findLCA(3, 4).data);
+        assertEquals("LCA(3, 4) = ", expected, tree.findLCA(3, 4).data);
         expected = 2;
-        Assert.assertEquals("LCA(2, 4) = ", expected, tree.findLCA(2, 4).data);
+        assertEquals("LCA(2, 4) = ", expected, tree.findLCA(2, 4).data);
 
         tree = new LCA();
-        tree.root = new Node(1);
-        Assert.assertEquals(1, tree.findLCA(1, 3).data);
+        tree.root = new TreeNode(1);
+        expected = 1;
+        assertEquals(expected, tree.findLCA(1, 3).data);
+        tree.root.left = new TreeNode(2);
+        assertEquals(expected, tree.findLCA(1, 2).data);
+
     }
     @Test
     public void testNull(){
         LCA tree = new LCA();
-        Assert.assertEquals(null, tree.findLCA(1, 2));
+        assertEquals(null, tree.findLCA(1, 2));
+    }
+
+    @Test
+    public void sameNodeTest() {
+        LCA sameNode = new LCA();
+        sameNode.root = new TreeNode(3);
+        sameNode.root.left = new TreeNode(3);
+        sameNode.root.right = new TreeNode(3);
+        int expected = 3;
+        assertEquals("LCA of 3 and 3: ",expected,sameNode.findLCA(3, 3).data);
     }
 }
 
