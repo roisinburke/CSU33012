3f8b6c7881b2c5bb213b35cda0de3d3937a05b2e
roisinburke <burker9@tcd.ie>
2020-11-05 14:00:09 +0000

    Changes to LCA and LCA Test file
    Changed variable name of TreeNode to Node.
    Changed the LCA solution to cover the DAG

--- LCA Java/LCA.java
+++ LCA Java/LCA.java
@@ -1,34 +1,58 @@
+import java.util.*;
+
 public class LCA {
 
-    TreeNode root;
 
-    TreeNode findLCA(int n1, int n2)
-    {
-        return findLCA(root, n1, n2);
+    Node root;
+    private List<Integer> firstRoute = new ArrayList<Integer>();
+    private List<Integer> secondRoute = new ArrayList<Integer>();
+
+    int findLCA(int node1, int node2) {
+        firstRoute.clear();
+        secondRoute.clear();
+
+        return findInsideLCA(root, node1, node2);
     }
 
-    TreeNode findLCA(TreeNode node, int n1, int n2)
-    {
+    private int findInsideLCA(Node root2, int node1, int node2) {
 
-        if (node == null)
-            return null;
+        if (!findRoute(root, node1, firstRoute) || !findRoute(root, node2, secondRoute)) {
+            return -1;
+        }
 
-        if (node.data == n1 || node.data == n2)
-            return node;
+        int count;
+        for (count = 0; count < firstRoute.size() && count < secondRoute.size(); count++) {
+            if (!firstRoute.get(count).equals(secondRoute.get(count)))
+                break;
+        }
 
-            TreeNode left_lca = findLCA(node.left, n1, n2);
-            TreeNode right_lca = findLCA(node.right, n1, n2);
+        return firstRoute.get(count - 1);
+    }
 
-        if (left_lca!=null && right_lca!=null)
-            return node;
+    private boolean findRoute(Node root3, int node, List<Integer> route) {
 
-        return (left_lca != null) ? left_lca : right_lca;
+        if (root3 == null) {
+            return false;
+        }
+        route.add(root3.data);
+
+        if (root3.data == node) {
+            return true;
+        }
+        if (root3.left != null && findRoute(root3.left, node, route)) {
+            return true;
+        }
+        if (root3.right != null && findRoute(root3.right, node, route)) {
+            return true;
+        }
+        route.remove(route.size() - 1);
+        return false;
     }
 }
-class TreeNode {
+class Node {
     int data;
-    TreeNode left, right;
-    public TreeNode(int item) {
+    Node left, right;
+    public Node(int item) {
         data = item;
         left = right = null;
     }
--- LCA Java/LCAJavaTest.java
+++ LCA Java/LCAJavaTest.java
@@ -1,78 +1,78 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
 
 public class LCAJavaTest {
     @Test
-    public void testLCA(){
+    public void testBST(){
         LCA tree = new LCA();
-        tree.root = new TreeNode(1);
-        tree.root.left = new TreeNode(2);
-        tree.root.right = new TreeNode(3);
-        tree.root.left.left = new TreeNode(4);
-        tree.root.left.right = new TreeNode(5);
-        tree.root.right.left = new TreeNode(6);
-        tree.root.right.right = new TreeNode(7);
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+        tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(5);
+        tree.root.right.left = new Node(6);
+        tree.root.right.right = new Node(7);
 
         int expected = 2;
-        assertEquals("LCA(4, 5) = ", expected, tree.findLCA(4, 5).data);
+        assertEquals("LCA(4, 5) = ", expected, tree.findLCA(4, 5));
         expected = 1;
-        assertEquals("LCA(4, 6) = " , expected, tree.findLCA(4, 6).data);
+        assertEquals("LCA(4, 6) = " , expected, tree.findLCA(4, 6));
         expected = 1;
-        assertEquals("LCA(3, 4) = ", expected, tree.findLCA(3, 4).data);
+        assertEquals("LCA(3, 4) = ", expected, tree.findLCA(3, 4));
         expected = 2;
-        assertEquals("LCA(2, 4) = ", expected, tree.findLCA(2, 4).data);
+        assertEquals("LCA(2, 4) = ", expected, tree.findLCA(2, 4));
 
         tree = new LCA();
-        tree.root = new TreeNode(1);
+        tree.root = new Node(1);
+        expected = -1;
+        assertEquals(expected, tree.findLCA(1, 3));
+        tree.root.left = new Node(2);
         expected = 1;
-        assertEquals(expected, tree.findLCA(1, 3).data);
-        tree.root.left = new TreeNode(2);
-        assertEquals(expected, tree.findLCA(1, 2).data);
+        assertEquals(expected, tree.findLCA(1, 2));
 
     }
-    
     @Test
     public void testNull(){
         LCA tree = new LCA();
-        assertEquals(null, tree.findLCA(1, 2));
+        int expected = -1;
+        assertEquals(expected, tree.findLCA(1, 2));
     }
-
     @Test
     public void testOddTree () {
-        BST oddTree = new BST();
-        oddTree.root = new TreeNode(1);
-        oddTree.root.right = new TreeNode(2);
-        oddTree.root.right.right = new TreeNode(3);
-        oddTree.root.right.right.right = new TreeNode(4);
-        oddTree.root.right.right.right.right = new TreeNode(5);
+        LCA oddTree = new LCA();
+        oddTree.root = new Node(1);
+        oddTree.root.right = new Node(2);
+        oddTree.root.right.right = new Node(3);
+        oddTree.root.right.right.right = new Node(4);
+        oddTree.root.right.right.right.right = new Node(5);
 
         int expected = 1;
-        assertEquals("Odd tree with five nodes: LCA(1, 2) = ", expected, oddTree.findLCA(1, 2).data);
+        assertEquals("Odd tree with five nodes: LCA(1, 2) = ", expected, oddTree.findLCA(1, 2));
         expected = 3;
-        assertEquals("Odd tree with five nodes: LCA(3, 5) = ", expected, oddTree.findLCA(3, 5).data);
+        assertEquals("Odd tree with five nodes: LCA(3, 5) = ", expected, oddTree.findLCA(3, 5));
         expected = 2;
-        assertEquals("Odd tree with five nodes: LCA(2, 4) = ", expected, oddTree.findLCA(2, 4).data);
-    }
+        assertEquals("Odd tree with five nodes: LCA(2, 4) = ", expected, oddTree.findLCA(2, 4));
 
+    }
     @Test
     public void testEvenTree () {
-        BST evenTree = new BST();
-        evenTree.root = new TreeNode(1);
-        evenTree.root.right = new TreeNode(2);
-        evenTree.root.right.right = new TreeNode(3);
-        evenTree.root.right.right.right = new TreeNode(4);
+        LCA evenTree = new LCA();
+        evenTree.root = new Node(1);
+        evenTree.root.right = new Node(2);
+        evenTree.root.right.right = new Node(3);
+        evenTree.root.right.right.right = new Node(4);
         int expected = 3;
-        assertEquals("Even tree with four nodes:  LCA(3, 4) = ", expected, evenTree.findLCA(3,4).data);
+        assertEquals("Even tree with four nodes:  LCA(3, 4) = ", expected, evenTree.findLCA(3,4));
         expected = 1;
-        assertEquals("Even tree with four nodes:  LCA(2, 1) = ", expected, evenTree.findLCA(2, 1).data);
+        assertEquals("Even tree with four nodes:  LCA(2, 1) = ", expected, evenTree.findLCA(2, 1));
     }
-
     @Test
     public void sameNodeTest() {
         LCA sameNode = new LCA();
-        sameNode.root = new TreeNode(3);
-        sameNode.root.left = new TreeNode(3);
-        sameNode.root.right = new TreeNode(3);
+        sameNode.root = new Node(3);
+        sameNode.root.left = new Node(3);
+        sameNode.root.right = new Node(3);
         int expected = 3;
-        assertEquals("LCA of 3 and 3: ",expected,sameNode.findLCA(3, 3).data);
+        assertEquals("LCA of 3 and 3: ",expected,sameNode.findLCA(3, 3));
     }
 }
-
