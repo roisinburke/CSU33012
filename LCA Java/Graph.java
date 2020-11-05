a1d37c57cc20ac8c4bc30b2814d2fa1c1c3e1ec0
roisinburke <burker9@tcd.ie>
2020-11-05 13:27:56 +0000

    Changes to Graph and Graph Test
    Added functions to Graph and removed unused private variable.
    Added constructor tests to GraphTest

--- LCA Java/Graph.java
+++ LCA Java/Graph.java
@@ -1,15 +1,11 @@
 import java.util.ArrayList;
 
 public class Graph {
+
     private int V;						//no. vertices
     private int E;						//no. edges
     private ArrayList<Integer>[] adj;   //adj[V] = adjacency list for vertex V
     private int [] indegree;			//indegree[V] = indegree of vertex V
-    private int [] outdegree;			//outdegree[V] = outdegree of vertex V
-    private boolean marked [];			//list of visited vertices
-    private boolean hasCycle;			//True if graph has cycle
-    private boolean stack [];			//
-
 
     public Graph(int V)
     {
@@ -21,8 +17,6 @@ public class Graph {
         this.V = V;
         this.E = 0;
         indegree = new int[V];
-        marked = new boolean[V];
-        stack = new boolean[V];
         adj = (ArrayList<Integer>[]) new ArrayList[V];
 
         for(int v = 0; v < V; v++)
@@ -41,4 +35,59 @@ public class Graph {
         return E;
     }
 
+    public void addEdge(int v, int w)
+    {
+        if((validateVertex(v) > 0) && (validateVertex(w) > 0))
+        {
+            adj[v].add(w);
+            indegree[w]++;
+            E++;
+        }
+        else
+        {
+            System.out.println("Enter numbers between 0 and " + (V-1));
+        }
+    }
+
+    private int validateVertex(int v)
+    {
+        if(v < 0 || v >= V)
+        {
+            return -1;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+
+    public int inDegree(int v)
+    {
+        if(validateVertex(v) > 0)
+        {
+            return indegree[v];
+        }
+        else
+        {
+            return -1;
+        }
+
+    }
+
+    public int outDegree(int v)
+    {
+        if(validateVertex(v) > 0)
+        {
+            return adj[v].size();
+        }
+        else
+        {
+            return -1;
+        }
+    }
+
+    public Iterable<Integer> adj(int v)
+    {
+        return adj[v];
+    }
 }
--- LCA Java/GraphTest.java
+++ LCA Java/GraphTest.java
@@ -1,3 +1,23 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
 public class GraphTest {
     
+    @Test
+    public void testGraph()
+    {
+        Graph graph = new Graph(10);
+
+        graph.addEdge(1, 2);
+        graph.addEdge(2, 4);
+        graph.addEdge(2, 5);
+        graph.addEdge(4, 6);
+        graph.addEdge(4, 7);
+
+        assertEquals(1, graph.inDegree(4));
+        assertEquals(2, graph.outDegree(4));
+        assertEquals(5, graph.E());
+        assertEquals(10, graph.V());
+        String adj = "[6, 7]";
+        assertEquals(adj, graph.adj(4).toString());
+    }
 }
